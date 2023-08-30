df78e0a6c67519e378f9540b0ca5d763becbc69d
annona17 <luna207sn@gmail.com>
2023-08-31 01:05:51 +0700

    Init Screen

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame, sys
 from pygame.locals import *
 
+WINDOWWIDTH = 850
+WINDOWHEIGHT = 600
+BACKGROUND = pygame.image.load('img/background.jpg')
+
 pygame.init()
+FPS = 60
+fpsClock = pygame.time.Clock()
 
-DISPLAYSURF = pygame.display.set_mode((700,600))
-pygame.display.set_caption('lna')
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
+pygame.display.set_caption('Flappy bird')
 
-while True: 
-    for event in pygame.event.get(): 
-        if event.type == QUIT: 
-            pygame.quit()
-            sys.exit()
-            
-    
+def main(): 
+    while True: 
+        for event in pygame.event.get(): 
+            if event.type == QUIT: 
+                pygame.quit()
+                sys.exit()
+                
+        DISPLAYSURF.blit(BACKGROUND, (0,0))
+        pygame.display.update()
+        fpsClock.tick(FPS)
+        
+if __name__ == '__main__': 
+    main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+DISPLAYSURF.fill((255, 255, 255))
+    pygame.draw.rect(DISPLAYSURF, (255, 255, 0), (100, 80, 150, 50) )
+    pygame.display.update()
